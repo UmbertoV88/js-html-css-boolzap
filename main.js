5b86c95b1148ea73a238252a2bb7282a966a16f4
UmbertoV <umberto.ottone@gmail.com>
2019-12-05 12:53:17 +0100

    JS funzionante - messaggi inviati funzionanti -cambio icona con focus funzionante

---
+++ main.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    var lunghezzaInput = $(".new-message-inputs").val();
+
+
+    $(".right-write-message-icon").click(function(){
+
+        invioMessaggio();
+
+    });
+
+    $(".new-message-inputs").keypress(function(event){
+        if(event.which == 13) {
+            invioMessaggio();
+        };
+    });
+
+    $(".new-message-inputs").focus(function (){
+        $(".fa-microphone").addClass("disactive")
+        $(".fa-paper-plane").removeClass("disactive")
+
+    })
+    $(".new-message-inputs").blur(function (){
+        $(".fa-microphone").removeClass("disactive")
+        $(".fa-paper-plane").addClass("disactive")
+
+    })
+
+});
+
+function invioMessaggio() {
+    var mexText = $(".new-message-inputs").val();
+    if (mexText.length != 0) {
+        var spazioCasella = $(".message.disactive.send").clone();
+        spazioCasella.removeClass("disactive");
+        spazioCasella.children(".testo-messaggio").text(mexText);
+        $(".read-message").append(spazioCasella);
+        $(".new-message-inputs").val('');
+
+    };
+};
